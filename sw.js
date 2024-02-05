549ce2693c58fcc39de4a9c06ce99e2eb84888e7
dony0720 <97350693+dony0720@users.noreply.github.com>
2024-02-05 11:39:42 +0900

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-6c6f29aaf4cc5ceb570b.js"
+    "url": "webpack-runtime-bd3f47323062ddfb83d1.js"
   },
   {
     "url": "styles.403d7a8a431a9dbd54c6.css"
@@ -40,7 +40,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "0cbee41701458d8645c6df6212ca16f5"
+    "revision": "b6c6174d92fac7b3d7a048b179d4a67b"
   },
   {
     "url": "static/webfonts/s/roboto/v30/KFOlCnqEu92Fr1MmSU5fBBc4.woff2"
